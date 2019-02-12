df5015ded05a257d4ebdb3dd7ccf803da373c90d
Aarav Gupta <aarav.gupta@6dtech.co.in>
2019-02-12 16:16:15 +0530

    blog fix

--- djangobl/settings.py
+++ djangobl/settings.py
@@ -52,13 +52,13 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        # 'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
-        # 'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     # 'DEFAULT_AUTHENTICATION_CLASSES': [],
